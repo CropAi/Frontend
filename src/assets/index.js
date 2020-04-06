adcad28ae7f78331b34f17f88478d16001a49fdf
satyam3976 <chaudhary.satyam3976@gmail.com>
2020-04-06 17:32:13 +0530

    changes done

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,15 +8,17 @@ const show_landing_container = () => {
     landingContainer.classList.remove("hidden");
     inputContainer.classList.add("hidden");
     resultContainer.classList.add("hidden");
-
 } 
+
+const analyze_click = () => {
+    window.history.pushState('Analyze', 'Crop Analysis', '?q=analyze');
+    show_input_container();
+}
 
 const show_input_container = () => {
     landingContainer.classList.add("hidden");
     inputContainer.classList.remove("hidden");
     resultContainer.classList.add("hidden");
-    var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?q=analyze';  // Append '/analyze' to url when analyze button is clicked
-    window.history.pushState({ path: newurl }, '', newurl);
 }
 
 const show_result_container = () => {
