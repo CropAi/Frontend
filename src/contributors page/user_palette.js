c84703ac246596b1d007d559b55bfb8c4b51f99b
Annu <annujolly17@gmail.com>
2020-04-17 17:05:43 +0530

    Update user_palette.js

--- src/contributors page/user_palette.js
+++ src/contributors page/user_palette.js
@@ -1,12 +1,23 @@
 const user_palette = [
-{
-	background: "cornflowerblue",
-	color: "white"
-},
-{
-	background: "orange",
-	color: "black"
-}
-
-
-]
+	{
+		background: "cornflowerblue",
+		color: "white"
+	},
+	{
+		background: "orange",
+		color: "black"
+	},
+	{
+		background: "#e03131",
+		color: "red"
+	},
+	{
+		background: "#4263eb",
+		color: "blue"
+	},
+	{
+		background: "#0c8599",
+		color: "Cyan"
+	}
+	
+	]
