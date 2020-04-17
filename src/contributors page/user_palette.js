8c29e7fa5532d70f8e75919550ccb150ad73157b
adyasha <adyashamohanty8104@gmail.com>
2020-04-17 22:53:31 +0530

    merge conflict

--- src/contributors page/user_palette.js
+++ src/contributors page/user_palette.js
@@ -1,16 +1,28 @@
 const user_palette = [
-{
-	background: "cornflowerblue",
-	color: "white"
-},
-{
-	background: "orange",
-	color: "black"
-},
-{
-    background: "SlateBlue",
-	color: "white"
-}
+    {
+        background: "SlateBlue",
+	    color: "white"
+    },
 
-
-]
+	{
+		background: "cornflowerblue",
+		color: "white"
+	},
+	{
+		background: "orange",
+		color: "black"
+	},
+	{
+		background: "#e03131",
+		color: "white"
+	},
+	{
+		background: "#4263eb",
+		color: "white"
+	},
+	{
+		background: "#0c8599",
+		color: "white"
+	}
+	
+	]
